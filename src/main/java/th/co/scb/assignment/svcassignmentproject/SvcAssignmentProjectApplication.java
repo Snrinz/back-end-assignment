7ca1938a1c3df2103556912d12d3e57c43af2c3a
Snrinz <pilyn24@hotmail.com>
2022-06-17 18:36:12 +0700

    Intitial a project

---
+++ src/main/java/th/co/scb/assignment/svcassignmentproject/SvcAssignmentProjectApplication.java
@@ -0,0 +1,16 @@
+package th.co.scb.assignment.svcassignmentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcAssignmentProjectApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		SpringApplication.run(SvcAssignmentProjectApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/th/co/scb/assignment/svcassignmentproject/SvcAssignmentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package th.co.scb.assignment.svcassignmentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcAssignmentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
